f6e60b8b3c110ab26eb204fdcac0f52947efc2b5
이나경 <leena3823@gmail.com>
2025-04-05 21:23:03 +0900

    ♻️ Refactor: Add 페이지 컴포넌트 분리 및 구조 개선
    - Information.tsx → Add.tsx로 리네이밍
    - Add 페이지 내 로직을 역할 기반으로 4개 컴포넌트로 분리
      - InputSection.tsx
      - GenderSelector.tsx
      - IndustryDropdown.tsx
      - Add.tsx (상위 컨테이너)

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Home from './pages/home/Home';
 import { GlobalStyle } from './styles/globalStyle';
 import './index.css';
 import Footer from './components/Footer';
-import Language from './pages/Language';
-import Information from './pages/Information';
-import Splash from './pages/Splash';
+import Language from './pages/language/Language';
+import Add from './pages/add/Add';
+import Splash from './pages/splash/Splash';
 
 function App() {
   const isSplash = window.location.pathname === '/';
+  const isAdd = window.location.pathname === '/add';
 
   return (
     <div className="w-[470px] min-h-screen flex flex-col bg-[#FDFDFD] relative mx-auto">
@@ -17,11 +18,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/language" element={<Language />} />
-          <Route path="/information" element={<Information />} />
+          <Route path="/add" element={<Add />} />
           <Route path="/" element={<Splash />} />
           <Route path="/home" element={<Home />} />
         </Routes>
-        {!isSplash && <Footer />}
+        {!isSplash && !isAdd && <Footer />}
       </Router>
     </div>
   );

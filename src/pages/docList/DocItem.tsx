f602161b6a69548c391e5057059819126a720cfc
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-04-13 15:46:26 +0900

    ✨ Feat: 전자문서 상세페이지 다국어 지원 구현
    * feat: 한국어 구현 완료
    
    * feat: 영어, 베트남어, 중국어 구현 완료

--- src/pages/docList/DocItem.tsx
+++ src/pages/docList/DocItem.tsx
@@ -1,13 +1,13 @@
 import { DocItem } from './InfoList';
 
 export const jehaejaInfo: DocItem[] = [
-  { label: '이름', value: '김성헌' },
-  { label: '외국인등록번호', value: '111111-111111' },
-  { label: '연락처', value: '010-1111-1111' },
-  { label: '성별', value: '남자' },
-  { label: '근로자유형', value: '근로자' },
+  { label: 'docDetail.label.name', value: '김성헌' },
+  { label: 'docDetail.label.id', value: '111111-111111' },
+  { label: 'docDetail.label.phone', value: '010-1111-1111' },
+  { label: 'docDetail.label.gender', value: '남자' },
+  { label: 'docDetail.label.workerType', value: '근로자' },
   {
-    label: '거주지',
+    label: 'docDetail.label.address',
     value: (
       <>
         서울특별시 광진구 <br /> 화양동 10-29
@@ -17,13 +17,13 @@ export const jehaejaInfo: DocItem[] = [
 ];
 
 export const companyInfo: DocItem[] = [
-  { label: '사업장관리번호', value: '김성헌' },
-  { label: '주소', value: '11111-111111' },
-  { label: '연락처', value: '010-1111-1111' },
-  { label: '사업주명', value: '남자' },
-  { label: '사업장명', value: '근로자' },
+  { label: 'docDetail.label.companyId', value: '김성헌' },
+  { label: 'docDetail.label.address', value: '11111-111111' },
+  { label: 'docDetail.label.phone', value: '010-1111-1111' },
+  { label: 'docDetail.label.ceo', value: '남자' },
+  { label: 'docDetail.label.companyName', value: '근로자' },
   {
-    label: '주소',
+    label: 'docDetail.label.address',
     value: (
       <>
         서울특별시 광진구 <br /> 화양동 10-29
@@ -33,13 +33,13 @@ export const companyInfo: DocItem[] = [
 ];
 
 export const injuryInfo: DocItem[] = [
-  { label: '유형', value: '업무상 사고' },
-  { label: '발생일시', value: '25.02.21' },
-  { label: '다친 부위', value: '010-1111-1111' },
-  { label: '의료기관', value: '서울병원' },
-  { label: '치료 구분', value: '통원' },
+  { label: 'docDetail.label.type', value: '업무상 사고' },
+  { label: 'docDetail.label.date', value: '25.02.21' },
+  { label: 'docDetail.label.part', value: '왼쪽 손가락' },
+  { label: 'docDetail.label.hospital', value: '서울병원' },
+  { label: 'docDetail.label.treatmentType', value: '통원' },
   {
-    label: '재해발생 경위',
+    label: 'docDetail.label.detail',
     value: (
       <>
         서울특별시 광진구 <br /> 화양동 10-29

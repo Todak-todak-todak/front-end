608c472e5c75fba0516a87512698ae4566c216f2
이나경 <leena3823@gmail.com>
2025-04-15 23:18:59 +0900

    feat: 병원 리스트 다국어 지원 구현

--- src/pages/result/KakaoMap/HosList.tsx
+++ src/pages/result/KakaoMap/HosList.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header';
 import KakaoMap from './KakaoMap';
 import HosInfo from './HosInfo';
+import { useTranslation } from 'react-i18next';
 
 const HosList = () => {
+  const { t } = useTranslation();
   return (
     <>
-      <Header title="산재 인정 병원" />
+      <Header title={t('result.hoslistTitle')} />
       <div className="pl-[25px] pr-[25px]">
         <KakaoMap />
         <HosInfo />

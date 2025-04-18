4b2c896a2105beba9a1b4a3dfa30ee56649520bd
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-04-18 14:40:58 +0900

    ✨ Feat: 추가정보 입력 api 연결 로직 수정
    * feat: api 공용 인스턴스 api.tsx 구현
    
    * feat: 추가 정보 입력하기 api 구현
    
    * feat: 추가 정보 입력 api 연결
    
    * feat: api 연결 로직 수정

--- src/apis/api.tsx
+++ src/apis/api.tsx
@@ -2,13 +2,12 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://todak-back-end.store/api/v1',
-  withCredentials: true, // 쿠키 필요 시 유지
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken');
   if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+    config.headers['Authorization'] = `Bearer ${token.trim()}`;
   }
   config.headers['Content-Type'] = 'application/json';
   return config;

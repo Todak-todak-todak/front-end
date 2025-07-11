d54c47e96ce69e27d971436d81b9872e75c7112c
이나경 <leena3823@gmail.com>
2025-07-11 17:48:04 +0900

    feat: 로딩 스피너 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
@@ -7,6 +7,7 @@ import './i18n';
 import i18n from './i18n';
 import router from './Router';
 import extractToken from './utils/auth/extractToken';
+import LoadingSpinner from './components/loading/LoadingSpinner';
 
 const savedLang = localStorage.getItem('lang') || 'ko';
 i18n.changeLanguage(savedLang);
@@ -21,7 +22,9 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="flex flex-col bg-[#FDFDFD] w-full max-w-[470px] mx-auto overflow-hidden">
-        <RouterProvider router={router} />
+        <Suspense fallback={<LoadingSpinner />}>
+          <RouterProvider router={router} />
+        </Suspense>
       </div>
     </QueryClientProvider>
   );

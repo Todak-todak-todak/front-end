17f403542dd1bc3929fca07fb165b7855179400d
이나경 <leena3823@gmail.com>
2025-07-11 18:05:57 +0900

    fix: 푸터 레이아웃 스타일 수정

--- src/layouts/FooterLayout.tsx
+++ src/layouts/FooterLayout.tsx
@@ -4,11 +4,13 @@ import Footer from '@/components/footer/Footer';
 export default function FooterLayout() {
   return (
     <div>
-      <div className="flex-1 pb-24">
-        <Outlet />
-      </div>
-      <div className="mt-auto z-[10000]">
-        <Footer />
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-1 pb-24">
+          <Outlet />
+        </div>
+        <div className="mt-auto z-50">
+          <Footer />
+        </div>
       </div>
     </div>
   );
